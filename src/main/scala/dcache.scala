ab04a0063538d9a46174769d165f8a437ff9ed2a
kinzafatim <kinzaf54@gmail.com>
2025-10-04 17:32:43 +0500

    extra

--- src/main/scala/dcache.scala
+++ src/main/scala/dcache.scala
@@ -1,30 +1,30 @@
-package cachematic
+// package cachematic
 
-import chisel3._
-import chisel3.util._
-import cachematic.types._
+// import chisel3._
+// import chisel3.util._
+// import cachematic.types._
 
-class DCacheIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
-  val addr      = Input(UInt(addrWidth.W))
-  val readEn    = Input(Bool())
-  val writeEn   = Input(Bool())
-  val writeData = Input(UInt(dataWidth.W))
-  val ready     = Output(Bool())
-  val readData  = Output(UInt(dataWidth.W))
-}
+// class DCacheIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
+//   val addr      = Input(UInt(addrWidth.W))
+//   val readEn    = Input(Bool())
+//   val writeEn   = Input(Bool())
+//   val writeData = Input(UInt(dataWidth.W))
+//   val ready     = Output(Bool())
+//   val readData  = Output(UInt(dataWidth.W))
+// }
 
-class DCache(cacheSizeBytes: Int, blockSizeBytes: Int, addrWidth: Int = 32, dataWidth: Int = 32) extends Module {
-  val io = IO(new DCacheIO(addrWidth, dataWidth))
+// class DCache(cacheSizeBytes: Int, blockSizeBytes: Int, addrWidth: Int = 32, dataWidth: Int = 32) extends Module {
+//   val io = IO(new DCacheIO(addrWidth, dataWidth))
 
-  val cache = Module(new DirectMappedCache(cacheSizeBytes, blockSizeBytes))
+//   val cache = Module(new DirectMappedCache(cacheSizeBytes, blockSizeBytes))
 
-  // inputs
-  cache.io.address   := io.addr
-  cache.io.readEn    := io.readEn
-  cache.io.writeEn   := io.writeEn
-  cache.io.writeData := io.writeData
+//   // inputs
+//   cache.io.address   := io.addr
+//   cache.io.readEn    := io.readEn
+//   cache.io.writeEn   := io.writeEn
+//   cache.io.writeData := io.writeData
 
-  //  outputs
-  io.readData := cache.io.readData
-  io.ready    := cache.io.hit // or use a stall/valid
-}
+//   //  outputs
+//   io.readData := cache.io.readData
+//   io.ready    := cache.io.hit // or use a stall/valid
+// }
--- src/main/scala/icache.scala
+++ src/main/scala/icache.scala
@@ -1,27 +1,27 @@
-package cachematic
+// package cachematic
 
-import chisel3._
-import chisel3.util._
-import cachematic.types._
+// import chisel3._
+// import chisel3.util._
+// import cachematic.types._
 
-class ICacheIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
-  val addr     = Input(UInt(addrWidth.W))
-  val valid    = Input(Bool()) // request valid
-  val ready    = Output(Bool()) // cache ready
-  val instr    = Output(UInt(dataWidth.W))
-}
+// class ICacheIO(val addrWidth: Int, val dataWidth: Int) extends Bundle {
+//   val addr     = Input(UInt(addrWidth.W))
+//   val valid    = Input(Bool()) // request valid
+//   val ready    = Output(Bool()) // cache ready
+//   val instr    = Output(UInt(dataWidth.W))
+// }
 
-class ICache(cacheSizeBytes: Int, blockSizeBytes: Int, addrWidth: Int = 32, dataWidth: Int = 32) extends Module {
-  val io = IO(new ICacheIO(addrWidth, dataWidth))
+// class ICache(cacheSizeBytes: Int, blockSizeBytes: Int, addrWidth: Int = 32, dataWidth: Int = 32) extends Module {
+//   val io = IO(new ICacheIO(addrWidth, dataWidth))
 
-  val cache = Module(new DirectMappedCache(cacheSizeBytes, blockSizeBytes))
-    //  inputs
-  cache.io.address := io.addr
-  cache.io.readEn  := io.valid
-  cache.io.writeEn := false.B
-  cache.io.writeData := 0.U
+//   val cache = Module(new DirectMappedCache(cacheSizeBytes, blockSizeBytes))
+//     //  inputs
+//   cache.io.address := io.addr
+//   cache.io.readEn  := io.valid
+//   cache.io.writeEn := false.B
+//   cache.io.writeData := 0.U
 
-  // outputs
-  io.instr := cache.io.readData
-  io.ready := cache.io.hit
-}
+//   // outputs
+//   io.instr := cache.io.readData
+//   io.ready := cache.io.hit
+// }
